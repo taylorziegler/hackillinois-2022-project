7f3954c3b375a397b1b928ab27b72fc9480d1d7e
Taylor <33166821+taylorziegler@users.noreply.github.com>
2022-02-27 01:16:01 -0600

    routes functional

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,26 @@
 from app import app
+from flask import jsonify
+from app.fractalseed import *
 
 @app.route('/')
 def home():
-    return "hello"
+    return str(SeedJulia(250)[1])
+@app.route('/GetJulia')
+def GetJulia():
+    temp = FetchTemperature()
+    values = SeedJulia(temp)
+    return jsonify({'x': values[0], 'y': values[1], 'color': values[2]})
+@app.route('/GetMandlebrot')
+def GetMandlebrot():
+    temp = FetchTemperature()
+    values = SeedMandlebrot(temp)
+    return jsonify({'color': values[0]})
+@app.route('/GetDailyFractal')
+def GetDailyFractal():
+    temp = FetchTemperature()
+    if (DailyFractal(temp) == "julia"):
+        values = SeedJulia(temp)
+        return jsonify({'type': 'julia', 'x': values[0], 'y': values[1], 'color': values[2]})
+    elif (DailyFractal(temp) == "mandlebrot"):
+        values = SeedMandlebrot(temp)
+        return jsonify({'type': 'mandlebrot', 'color': values[0]})
